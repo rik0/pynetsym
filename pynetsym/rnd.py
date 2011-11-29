f283fb0733d34ca6b679526420222e6b4450fe67
Enrico Franchi <efranchi@ce.unipr.it>
2011-11-29 21:44:08 +0100

    Slightly improved random node choice.

--- pynetsym/rnd.py
+++ pynetsym/rnd.py
@@ -1,21 +1,24 @@
 import random
 
-__author__ = 'enrico'
-
 def iter_random_choice(iterator, max_value):
-    choosen_index = random.randint(0, max_value)
+    chosen_index = random.randint(0, max_value)
     for index, item in enumerate(iterator):
-        if index == choosen_index:
+        if index == chosen_index:
             return item
 
 
 def random_edge(graph):
     return iter_random_choice(
-            graph.edges_iter(),
-            graph.number_of_edges())
+        graph.edges_iter(),
+        graph.number_of_edges())
 
 
 def random_node(graph):
-    return iter_random_choice(
+    max_value = graph.number_of_nodes()
+    chosen_index = random.randint(0, max_value)
+    if graph.has_node(chosen_index):
+        return chosen_index
+    else:
+        return iter_random_choice(
             graph.nodes_iter(),
-            graph.number_of_nodes())
+            max_value)
