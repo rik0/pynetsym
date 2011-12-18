0a97ac4ae9d0c5cf526b9604d26e5394c6db5e89
Enrico Franchi <efranchi@ce.unipr.it>
2011-12-18 10:46:19 +0100

    Added new test model files. Added program to print network features. Added function to load networks in io.

--- pynetsym/io.py
+++ pynetsym/io.py
@@ -1,18 +1,30 @@
 import networkx as nx
+from os import path
 
 FORMATS = ('dot', 'gexf', 'gml', 'gpickle',
            'graphml', 'pajek', 'yaml')
 
-def save_network(network, out, fmt):
-    if out is None and fmt is None:
+def save_network(network, network_path=None, fmt=None, **kwargs):
+    if network_path is None and fmt is None:
         return
-    elif out is None:
-        out = 'network'
+    elif network_path is None:
+        network_path = 'network'
     elif fmt is None:
-        out, ext = path.splitext(out)
+        network_path, ext = path.splitext(network_path)
         fmt = ext[1:]
         if fmt not in FORMATS:
-            print "Error"
-            return
-    fn = getattr(nx, 'write_' + fmt)
-    fn(network, out + '.' + fmt)
+            raise IOError("Did not undestand format from filename %s." % network_path)
+    try:
+        fn = getattr(nx, 'write_' + fmt)
+    except AttributeError:
+        raise ValueError("Could not save in %s format." % fnt)
+    fn(network, network_path + '.' + fmt, **kwargs)
+
+def read_network(network_path, fmt=None, **kwargs):
+    if fmt is None:
+        _, ext = path.splitext(network_path)
+        fmt = ext[1:]
+        if fmt not in FORMATS:
+            raise IOError("Did not undestand format from filename %s." % network_path)
+    fn = getattr(nx, 'read_' + fmt)
+    fn(network_path, **kwargs)
