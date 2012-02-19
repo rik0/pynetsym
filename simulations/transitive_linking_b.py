7eb500cc199ebc295902bcb04e748c57fc7c22e4
Enrico Franchi <efranchi@ce.unipr.it>
2012-02-19 11:12:11 +0100

    Added second variant of tl.

---
+++ simulations/transitive_linking_b.py
@@ -0,0 +1,42 @@
+from . import transitive_linking
+
+import random
+import itertools as it
+
+from pynetsym import simulation, rnd, choice_criteria, generation
+
+
+class Node(transitive_linking.Node):
+    def find_possible_links(self, graph, neighbors):
+        possible_links = [link for link in it.combinations(neighbors, 2)
+                          if not graph.has_edge(*link)]
+        return possible_links
+
+    def introduce(self):
+        graph = self.graph
+        neighbors = graph.neighbors(self.id)
+
+        possible_links = self.find_possible_links(graph, neighbors)
+        if possible_links:
+            node_a, node_b = random.choice(possible_links)
+            self.send(node_a, 'introduce_to', target_node=node_b)
+        else:
+            self.link_to(self.criterion)
+
+
+class TL(simulation.Simulation):
+    simulation_options = (
+        ('-n', '--network-size', dict(default=100, type=int)),
+        ('--death-probability', dict(default=0.01, type=float)),
+        ('--preferential-attachment', dict(
+            dest='criterion', action='store_const',
+            const=choice_criteria.preferential_attachment,
+            default=rnd.random_node)))
+
+    class configurator(generation.SingleNodeConfigurator):
+        node_cls = Node
+        node_options = {"death_probability", "criterion"}
+        activator_options = {}
+
+
+
