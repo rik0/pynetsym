ecd890098a7999b3ad5047ad6dc9ecadd92bb47c
Enrico Franchi <efranchi@ce.unipr.it>
2012-02-19 11:16:17 +0100

    Removed unused files.

--- pynetsym/models/transitive_linking_pa_b.py
+++
@@ -1,9 +0,0 @@
-import functools as ft
-from pynetsym.models import transitive_linking, transitive_linking_b, transitive_linking_pa
-
-
-class Node(transitive_linking_b.Node, transitive_linking_pa.Node):
-    pass
-
-make_parser = transitive_linking.make_parser
-make_setup = ft.partial(transitive_linking.make_setup, node_cls=Node)
