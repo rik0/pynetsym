63216de5a480f8605abc2100417542bbf8f293a3
Enrico Franchi <efranchi@ce.unipr.it>
2012-03-29 14:20:17 +0200

    Fixed bug: graph_options could be shared (in Simulation) between different simulation instances.

--- tests/test_run_transitive_linking.py
+++ tests/test_run_transitive_linking.py
@@ -7,23 +7,23 @@ class TestTL(unittest.TestCase):
     def setUp(self):
         self.args = [ "-n", 100, "--death-probability", 0.01]
     def testTL(self):
-        self.sim = transitive_linking.TL()
-        self.sim.run(args=self.args)
+        sim = transitive_linking.TL()
+        sim.run(args=self.args)
     def testTLB(self):
-        self.sim = transitive_linking_b.TL()
-        self.sim.run(args=self.args)
+        sim = transitive_linking_b.TL()
+        sim.run(args=self.args)
     def testTL_igraph(self):
-        self.sim = transitive_linking_igraph.TL()
-        self.sim.run(args=self.args)
+        sim = transitive_linking_igraph.TL()
+        sim.run(args=self.args)
     def testTL_pa(self):
         self.args.append('--preferential-attachment')
-        self.sim = transitive_linking.TL()
-        self.sim.run(args=self.args)
+        sim = transitive_linking.TL()
+        sim.run(args=self.args)
     def testTLB_pa(self):
         self.args.append('--preferential-attachment')
-        self.sim = transitive_linking_b.TL()
-        self.sim.run(args=self.args)
+        sim = transitive_linking_b.TL()
+        sim.run(args=self.args)
     def testTL_igraph_pa(self):
         self.args.append('--preferential-attachment')
-        self.sim = transitive_linking_igraph.TL()
-        self.sim.run(args=self.args)
+        sim = transitive_linking_igraph.TL()
+        sim.run(args=self.args)
