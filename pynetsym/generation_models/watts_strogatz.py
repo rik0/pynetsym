b960936ac6390ce1f7e887de7ca22c6ee4ccafc8
Enrico Franchi <efranchi@ce.unipr.it>
2012-03-30 16:43:38 +0200

    Fixed ws model.

--- pynetsym/generation_models/watts_strogatz.py
+++ pynetsym/generation_models/watts_strogatz.py
@@ -12,17 +12,23 @@ class Node(core.Node):
 
     @core.answers('activation_received', node_id='id')
     def activate(self):
-        graph = self.graph.handle
-        for node in graph.neighbors(self.id):
-            if random.random() < self.rewiring_probability:
-                self.graph.remove_edge(self.id, node)
+        for index in self.lattice_cw_neighbors():
+            neighbors = self.graph.neighbors(self.id)
+            if (random.random() < self.rewiring_probability
+                    and index in neighbors):
                 random_node = self.graph.random_node()
+                while random_node in neighbors:
+                    random_node = self.graph.random_node()
+                self.graph.remove_edge(self.id, index)
                 self.graph.add_edge(self.id, random_node)
+            self.cooperate()
 
+    def lattice_cw_neighbors(self):
+        return range(self.id + 1,
+                self.id + self.lattice_connections + 1)
 
     def initialize(self):
-        for index in range(self.id+1, 
-                           self.id+self.lattice_connections+1):
+        for index in self.lattice_cw_neighbors():
             self.link_to(index % self.network_size)
 
 class Activator(simulation.Activator):
