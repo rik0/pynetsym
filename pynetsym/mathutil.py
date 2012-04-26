db78d9de2a178f905b26f0902932cbc62e7d97ed
Enrico Franchi <enrico.franchi@gmail.com>
2012-04-26 15:32:35 +0200

    Cleaning code.

--- pynetsym/mathutil.py
+++ pynetsym/mathutil.py
@@ -1,20 +1,21 @@
 import math
 import random
-import networkx as nx
+import networkx
 from numpy import cumsum, sum
 
+
 def ccdf(dist):
-    return 1 - (cumsum(dist, dtype=float)/sum(dist))
+    return 1 - (cumsum(dist, dtype=float) / sum(dist))
 
 
 def estimate_s(q, delta, eps):
     delta2 = delta * delta
-    delta3 = (1-delta)*(1-delta)
-    return (2. / (q*q)) * math.log(2./eps) * delta3 / delta2
+    delta3 = (1 - delta) * (1 - delta)
+    return (2. / (q * q)) * math.log(2. / eps) * delta3 / delta2
 
 
 def approximate_cpl(graph, q=0.5, delta=0.15, eps=0.05):
-    assert isinstance(graph, nx.Graph)
+    assert isinstance(graph, networkx.Graph)
     s = estimate_s(q, delta, eps)
     s = int(math.ceil(s))
     if graph.number_of_nodes() <= s:
@@ -24,8 +25,8 @@ def approximate_cpl(graph, q=0.5, delta=0.15, eps=0.05):
 
     averages = []
     for node in sample:
-        path_lengths = nx.single_source_shortest_path_length(graph, node)
-        average = sum(path_lengths.itervalues())/float(len(path_lengths))
+        path_lengths = networkx.single_source_shortest_path_length(graph, node)
+        average = sum(path_lengths.itervalues()) / float(len(path_lengths))
         averages.append(average)
     averages.sort()
     median_index = int(len(averages) * q + 1)
