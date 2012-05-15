c04dc9f97bd32c902ac70a19548d15551e15e9e9
Enrico Franchi <efranchi@ce.unipr.it>
2012-05-15 19:42:36 +0200

    Started writing implementation of neo backend (for now copied and broken).

---
+++ pynetsym/storage/neo.py
@@ -0,0 +1,90 @@
+import types
+from pynetsym.storage.basic import GraphError, GraphWrapper
+
+try:
+    import neo4j
+except ImportError, neo4j_import_exception:
+    neo4j = types.ModuleType('neo4j', 'Fake module')
+
+    class Neo4JGraphWrapper(GraphWrapper):
+        def __init__(self):
+            raise neo4j_import_exception
+else:
+
+    db = neo4j.GraphDatabase()
+
+    class Neo4JWrapper(GraphWrapper):
+        def __init__(self, graph):
+            """
+            Creates an implementation of Graph backed with igraph.Graph
+            @param graph: the backing graph
+            @type graph: neo4j.GraphDatabase
+            """
+            self.graph = graph
+
+        def add_node(self, identifier, agent):
+            self.graph.node(value=identifier)
+
+
+            self.graph.add_vertices(1)
+            largest_index = len(self.graph.vs) - 1
+            self.graph.vs[largest_index]["identifier"] = identifier
+            self.graph.vs[largest_index]["agent"] = agent
+
+        def __getitem__(self, identifier):
+            return self.graph.vs[identifier]["agent"]
+
+        def add_edge(self, source, target):
+            self.graph.add_edges(((source, target), ))
+
+        def remove_edge(self, source, target):
+            self.graph.delete_edges(((source, target), ))
+
+        def random_node(self):
+            try:
+                return random.randrange(0, len(self.graph.vs))
+            except IndexError:
+                raise GraphError("Extracting node from empty graph.")
+
+        def random_edge(self):
+            try:
+                edge = random.choice(self.graph.es)
+                return edge.source, edge.target
+            except IndexError:
+                raise GraphError(
+                        "Extracting edge from graph with no edges")
+
+        def preferential_attachment_node(self):
+            no_vertices = len(self.graph.vs)
+            no_edges = len(self.graph.es)
+            try:
+                random_index = random.randrange(no_vertices + no_edges)
+            except IndexError:
+                raise GraphError("Extracting node from empty graph.")
+            else:
+                if random_index < no_vertices:
+                    return random_index
+                else:
+                    return self.graph.es[random_index - no_vertices].source
+
+        def remove_node(self, identifier):
+            raise NotImplementedError()
+
+        @property
+        def handle(self):
+            return self.graph
+
+        def __contains__(self, identifier):
+            if 0 < identifier < len(self.graph.vs):
+                return True
+            else:
+                return False
+
+        def neighbors(self, node_identifier):
+            return self.graph.neighbors(node_identifier)
+
+        def nodes(self):
+            indices = self.graph.vs.indices
+            for index in indices:
+                yield index, self.graph.vs[index].attributes()
+
