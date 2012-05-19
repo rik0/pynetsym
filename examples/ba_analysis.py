1cef03b87b61d17d2b67b008d4dff800909c1461
Enrico Franchi <enrico.franchi@gmail.com>
2012-05-19 17:39:58 +0200

    Created script to actually generate the test file.

--- examples/ba_analysis.py
+++ examples/ba_analysis.py
@@ -1,35 +1,55 @@
-from pynetsym import node_manager
-from pynetsym import simulation
-from pynetsym import storage
-from pynetsym.generation_models import barabasi_albert
+import os
+import time
+from os import path
+from matplotlib import pyplot as plt
 
-import igraph
+import networkx as nx
+
+from pynetsym.generation_models import barabasi_albert
+from pynetsym import mathutil
 
 Node = barabasi_albert.Node
 Activator = barabasi_albert.Activator
 BA = barabasi_albert.BA
 
-class BA(simulation.Simulation):
-    command_line_options = (
-        ('-n', '--network-size', dict(default=100, type=int)),
-        ('-m', '--starting-edges', dict(default=5, type=int)))
-
-    activator = Activator
-
-    graph_type = storage.IGraphWrapper
-    graph_options = dict(graph=igraph.Graph(0))
-
-    class configurator(node_manager.SingleNodeConfigurator):
-        node_cls = Node
-        node_options = {'starting_edges'}
+multipliers = [1, 10, 100, 1000]
+markers = ['+', 'x', '*', '.']
+colors = ['blue', 'red', 'green', 'cyan']
 
 if __name__ == '__main__':
-    for starting_size_multiplier in [1, 10, 100, 1000]:
+    directory_name = "ba_test_%d" % time.time()
+    os.mkdir(directory_name)
+
+    F = plt.figure(figsize=(10, 4))
+
+    for starting_size_multiplier, marker, color in zip(
+            multipliers, markers, colors):
         sim = BA()
-        sim.run(starting_edges=)
+        sim.setup_parameters()
+        sim.run(network_size=sim.starting_edges * starting_size_multiplier)
+
+        filename = path.join(
+            os.curdir,
+            directory_name,
+            "%(steps)s_%(network_size)s_%(starting_edges)s.graphml" % vars(sim))
+        sim.graph.handle.write(filename)
+
+        label = '%d starting size' % sim.network_size
+        del sim
+
+        graph = nx.read_graphml(filename)
+        dst = mathutil.degrees_to_hist(graph.degree())
+        ccdf = mathutil.ccdf(dst)
+
+        plt.subplot(121)
+        plt.loglog(dst, marker=marker, color=color, linestyle='',
+            label=label)
+
+        plt.subplot(122)
+        plt.loglog(ccdf, color=color, label=label)
+
+    plt.subplot(121)
+    plt.legend()
+    plt.savefig(path.join(os.curdir, directory_name, 'plt.png'),
+                dpi=1000, format='png')
     
-    
-    sim = BA()
-    sim.run()
-    sim.output_processor(igraph.Graph.write, 'ba_%d_%d_%d.graphml' %
-            (sim.network_size, sim.steps, sim.starting_edges))
