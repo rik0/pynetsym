f89d0bec44052c05e0774f11949904a2e2f7491e
Enrico Franchi <enrico.franchi@gmail.com>
2012-09-12 17:33:47 +0200

    Improved test. Added parametrized tests facility.

--- tests/test_sw.py
+++ tests/test_sw.py
@@ -1,26 +1,37 @@
 import unittest
+import paramunittest
+
 from pynetsym.generation_models import watts_strogatz
 import networkx as nx
 
+@paramunittest.parametrized(
+    (0.1, 2, 100),
+    (0.1, 2, 1000),
+    (0.5, 2, 100),
+    (0.5, 2, 1000),
+)
 class TestWS(unittest.TestCase):
-    def testRun(self):
-        rewiring_probability = 0.1
-        lattice_connections = 2
-        starting_network_size = 100
+    def setParameters(self, rewiring_probability, lattice_connections,
+                      starting_network_size):
+        self.rewiring_probability = rewiring_probability
+        self.lattice_connections = lattice_connections
+        self.starting_network_size = starting_network_size
 
+    def testRun(self):
         sim = watts_strogatz.WS()
         sim.run(
-            rewiring_probability=rewiring_probability,
-            lattice_connections=lattice_connections,
-            starting_network_size=starting_network_size)
+            rewiring_probability=self.rewiring_probability,
+            lattice_connections=self.lattice_connections,
+            starting_network_size=self.starting_network_size)
 
         graph = sim.graph.handle
         self.assertEqual(
-                starting_network_size,
-                graph.number_of_nodes())
+            self.starting_network_size,
+            graph.number_of_nodes())
         self.assertEqual(
-                starting_network_size * lattice_connections,
-                graph.number_of_edges())
+            self.starting_network_size * self.lattice_connections,
+            graph.number_of_edges())
         self.assertLess(nx.diameter(graph),
-                        starting_network_size / (lattice_connections * 2))
+                        self.starting_network_size / (
+                        self.lattice_connections * 2))
         self.assertLessEqual(nx.average_shortest_path_length(graph), 7)
