75f1ebfb717f6fdaf6a716e2aa4d916443a38775
Enrico Franchi <efranchi@ce.unipr.it>
2012-09-23 23:32:29 +0200

    Adding iter-friendly module.

---
+++ pynetsym/util/iterators.py
@@ -0,0 +1,22 @@
+
+def inits(iterable):
+    """
+    The inits function returns all initial segments of the argument, shortest first. For example,
+
+    >>> list(inits(""))
+    [[]]
+    >>> list(inits("abc"))
+    [[], ['a'], ['a', 'b'], ['a', 'b', 'c']]
+    """
+
+    parts = []
+
+    for item in iter(iterable):
+        yield list(parts)
+        parts.append(item)
+    yield list(parts)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
