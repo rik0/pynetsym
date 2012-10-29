98441133c20810eb7b443c3c11e3e54afe96f67c
Enrico Franchi <efranchi@ce.unipr.it>
2012-10-29 18:11:21 +0100

    Fixed some details on the true intended semantics of DefaultIndexMap.

--- pynetsym/graph/_util.py
+++ pynetsym/graph/_util.py
@@ -1,23 +1,44 @@
-
+import collections
+import numbers
+from numpy import isscalar, ndarray
+import functools as fc
+import itertools as it
 
 def identity(x):
     return x
 
+def issequence(item):
+    if (isinstance(item, collections.Sequence) or
+        (isinstance(item, ndarray) and item.ndim == 1)):
+        return True
+    else:
+        return False
+
+
 class DefaultIndexMap(object):
-    def __init__(self, dictionary={},
-            default_function=identity, **kwargs):
-        self.elements = {}
-        self.elements.update(dictionary, **kwargs)
+    def __init__(self, dictionary={}, default_function=identity):
+        if not all(isinstance(key, numbers.Integral) for key in dictionary.keys()):
+            raise TypeError('Some key is not integral')
+        self.elements = dict(dictionary)
         self.default_function = default_function
 
     def _get_one(self, item):
-        try:
-            return self.elements[item]
-        except KeyError:
+        if isinstance(item, numbers.Integral):
             try:
-                return self.default_function(item)
-            except Exception as e:
-                raise KeyError(e)
+                return self.elements[item]
+            except KeyError:
+                try:
+                    return self.default_function(item)
+                except Exception as e:
+                    raise KeyError(e)
+        else:
+            raise TypeError("Key %s is not an integer." % item)
 
     def __getitem__(self, item):
-        return self._get_one(item)
+        if issequence(item):
+            return [self._get_one(element) for element in item]
+        elif isinstance(item, slice):
+            stride = 1 if item.step is None else item.step
+
+        else:
+            return self._get_one(item)
--- tests/test_defaultIndexMap.py
+++ tests/test_defaultIndexMap.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from numpy import arange
 from paramunittest import parametrized, ParametrizedTestCase
 from pynetsym.graph._util import DefaultIndexMap
 
@@ -6,36 +7,30 @@ from pynetsym.graph._util import DefaultIndexMap
 class TestDefaultIndexMap(TestCase):
     def testEmpty(self):
         default_index_map = DefaultIndexMap()
-        for item in [1, 'a', 'foo', 5.6]:
+        for item in range(3):
             self.assertEqual(item, default_index_map[item])
 
-    def testWithValues(self):
-        default_index_map = DefaultIndexMap(a=1, b=2)
-        self.assertEqual(1, default_index_map['a'])
-        self.assertEqual(2, default_index_map['b'])
-        self.assertEqual(2, default_index_map[2])
+    def testInvalidKeys(self):
+        default_index_map = DefaultIndexMap()
+        self.assertRaises(TypeError, default_index_map.__getitem__, 'a')
+        self.assertRaises(TypeError, default_index_map.__getitem__, 'foo')
+        self.assertRaises(TypeError, default_index_map.__getitem__, 5.5)
 
-    def testWithValues2(self):
+    def testInvalidKeyInit(self):
+        self.assertRaises(TypeError, DefaultIndexMap, {'foo': 2, 2: 3})
+
+    def testWithValues(self):
         default_index_map = DefaultIndexMap({1: 2, 2: 3})
         self.assertEqual(2, default_index_map[1])
         self.assertEqual(3, default_index_map[2])
         self.assertEqual(3, default_index_map[3])
 
-    def testWithValues3(self):
-        default_index_map = DefaultIndexMap({1: 2, 2: 3}, a=1)
-        self.assertEqual(1, default_index_map['a'])
-        self.assertEqual(2, default_index_map[1])
-        self.assertEqual(3, default_index_map[2])
-        self.assertEqual(3, default_index_map[3])
-
-    def testWithValues3(self):
+    def testWithNonDefaultFunc(self):
         default_index_map = DefaultIndexMap(default_function=lambda x: x * 2,
-                                            dictionary={1: 2, 2: 3}, a=1)
-        self.assertEqual(1, default_index_map['a'])
+                                            dictionary={1: 2, 2: 3})
         self.assertEqual(2, default_index_map[1])
         self.assertEqual(3, default_index_map[2])
         self.assertEqual(6, default_index_map[3])
-        self.assertEqual('bb', default_index_map['b'])
 
 
 @parametrized(
@@ -44,10 +39,9 @@ class TestDefaultIndexMap(TestCase):
        [1, 3],
        [7, 8, 9],
        range(10),
-       range(3, 30, 5),
-       list('foobar')]]
+       range(3, 30, 5)]]
 )
-class TestMultipleIndexes(ParametrizedTestCase):
+class TestSequence(ParametrizedTestCase):
     def setParameters(self, lst):
         self.lst = lst
 
@@ -55,5 +49,62 @@ class TestMultipleIndexes(ParametrizedTestCase):
         self.index_map = DefaultIndexMap()
 
     def testSequence(self):
-        self.assertListEqual(
+        self.assertSequenceEqual(
             self.lst, self.index_map[self.lst])
+
+
+@parametrized(
+    *[(tuple(el), ) for el in
+      [[1, 2, 3],
+       [1, 3],
+       [7, 8, 9],
+       range(10),
+       range(3, 30, 5)]]
+)
+class TestSequenceWithMap(ParametrizedTestCase):
+    def setParameters(self, lst):
+        self.lst = lst
+
+    def setUp(self):
+        original_map = dict(zip(arange(5), -arange(5)))
+        self.mapped_lst = [-el if 0 <= el < 5 else el for el in self.lst]
+        self.index_map = DefaultIndexMap(original_map)
+
+    def testSequence(self):
+        self.assertSequenceEqual(
+            self.mapped_lst, self.index_map[self.lst])
+
+
+@parametrized(
+    (0, 3, 1),
+    (0, 3, 2),
+    (0, 3, -1),
+    (0, 3, -2),
+    (3, 0, -1),
+    (3, 0, -2),
+    (-10, 10, 1),
+    (10, -10, -2)
+)
+class TestSlices(ParametrizedTestCase):
+    def setParameters(self, start, stop, stride):
+        self.start = start
+        self.stop = stop
+        self.stride = stride
+
+    def setUp(self):
+        original_map = dict(zip(arange(5), -arange(5)))
+        self.index_map = DefaultIndexMap(original_map)
+
+    def testSliceFull(self):
+        mapped_lst = [-el if 0 <= el < 5 else el
+                      for el in range(self.start, self.stop, self.stride)]
+
+        self.assertSequenceEqual(
+            mapped_lst, self.index_map[self.start:self.stop:self.stride])
+
+    def testSliceNoStride(self):
+        mapped_lst = [-el if 0 <= el < 5 else el
+                      for el in range(self.start, self.stop, 1)]
+
+        self.assertSequenceEqual(
+            mapped_lst, self.index_map[self.start:self.stop])
