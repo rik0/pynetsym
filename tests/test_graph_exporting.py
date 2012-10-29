812f913e8bb58cdd4743f7b28699363d3031a955
Enrico Franchi <efranchi@ce.unipr.it>
2012-10-29 11:43:18 +0100

    Improving unittest.

--- tests/test_graph_exporting.py
+++ tests/test_graph_exporting.py
@@ -1,4 +1,6 @@
 import unittest
+from numpy import testing, array
+import numpy as np
 import paramunittest
 from pynetsym.graph import ScipyGraph, NxGraph
 import networkx as nx
@@ -21,8 +23,49 @@ class TestGraphExporting(paramunittest.ParametrizedTestCase):
         for target in xrange(1, self.number_of_initial_nodes):
             self.graph.add_edge(0, target)
 
-#        self.graph.remove_node(self.removed_node)
+        self.graph.remove_node(self.removed_node)
+
+        self.adjacency = np.matrix(
+            [[0, 1, 1, 0, 1],
+             [1, 0, 0, 0, 0],
+             [1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0],
+             [1, 0, 0, 0, 0]], dtype=bool)
+        self.minimized_adjacency = np.matrix(
+            [[0, 1, 1, 1],
+             [1, 0, 0, 0],
+             [1, 0, 0, 0],
+             [1, 0, 0, 0]], dtype=bool)
 
     def testNetworkX(self):
-        expected_graph = nx.star_graph(self.number_of_initial_nodes - 1)
-        self.assertTrue(nx.is_isomorphic(expected_graph, self.graph.to_nx()))
+        star_graph = nx.star_graph(self.number_of_initial_nodes - 1)
+        self.assertFalse(
+            nx.is_isomorphic(star_graph, self.graph.to_nx()))
+        star_graph.remove_node(self.removed_node)
+        self.assert_(nx.is_isomorphic(star_graph, self.graph.to_nx()))
+
+    def testNumpyNotMinimized(self):
+        testing.assert_array_equal(
+            self.adjacency,
+            self.graph.to_numpy(minimize=False))
+
+    def testScipyNotMinimized(self):
+        testing.assert_array_equal(
+            self.minimized_adjacency,
+            self.graph.to_scipy(minimize=False).todense())
+
+    def testScipyMinimized(self):
+        (matrix,
+         to_matrix_indices,
+         from_matrix_indices) = self.graph.to_scipy(minimize=True)
+        testing.assert_array_equal(
+            self.adjacency, matrix.todense())
+
+    def testNumpyMinimized(self):
+        (matrix,
+         to_matrix_indices,
+         from_matrix_indices) = self.graph.to_scipy(minimize=True)
+        testing.assert_array_equal(
+            self.minimized_adjacency,
+            matrix.todense())
+
