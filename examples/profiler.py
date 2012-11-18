d64bb669ad293c42b808e48fb70a9c70ba6d9984
Enrico Franchi <efranchi@ce.unipr.it>
2012-11-18 15:09:17 +0100

    Added helper for profiling.

---
+++ examples/profiler.py
@@ -0,0 +1,26 @@
+import sys
+import importlib
+import cProfile
+
+arguments = sys.argv[:]
+this_module = arguments.pop(0)
+
+try:
+    module_name = arguments.pop(0)
+    simulation_factory_name = arguments.pop(0)
+    out_name = arguments.pop(0)
+except IndexError:
+    print 'Missing arguments'
+    exit(-1)
+
+module = importlib.import_module(module_name)
+simulation_factory = getattr(module, simulation_factory_name)
+sim = simulation_factory()
+
+cProfile.run('sim.run(args=arguments)', out_name)
+# sim.run(args=arguments)
+
+
+with sim.graph.handle as graph:
+    print graph.number_of_nodes()
+    print graph.number_of_edges()
