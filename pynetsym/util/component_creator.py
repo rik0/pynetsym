e4a31d626669d4240eb86dea51e6c2a9dad9dce8
Enrico Franchi <efranchi@ce.unipr.it>
2012-11-19 20:45:03 +0100

    Introduced Component Creator.

---
+++ pynetsym/util/component_creator.py
@@ -0,0 +1,84 @@
+import inspect
+
+from pynetsym import error
+from .dictionary import extract_subdictionary
+
+
+
+class ComponentError(error.PyNetSymError):
+    pass
+
+class ComponentCreator(object):
+    def __init__(self, context, component_name):
+        self.context = context
+        self.component_name = component_name
+
+    @property
+    def factory_name(self):
+        return '%s_type' % (self.component_name, )
+
+    @property
+    def options_name(self):
+        return '%s_options' % (self.component_name, )
+
+    @property
+    def parameters_name(self):
+        return '%s_parameters' % (self.component_name, )
+
+    def factory(self):
+        factory = getattr(self.context, self.factory_name, None)
+        if factory is not None:
+            return factory
+        else:
+            raise ComponentError('Cannot create component %s' % (
+                self.component_name, ))
+
+    def process_arg_spec(self, arg_spec):
+        arg_names, more_arg_name, kw_name, defaults = arg_spec
+        if (more_arg_name is not None or
+             kw_name is not None):
+            raise ComponentError("Cannot use introspection with varargs or keyword args.")
+        return arg_names
+
+    def options(self, factory):
+        try:
+            return factory.options
+        except AttributeError:
+            try:
+                return getattr(self.context, self.options_name)
+            except AttributeError:
+                if inspect.isfunction(factory):
+                    arg_spec = inspect.getargspec(factory)
+                elif hasattr(factory, '__init__'):
+                    arg_spec = inspect.getargspec(factory.__init__)
+                else:
+                    raise ComponentError(
+                            'Cannot find options for %s' % (
+                                self.component_name, ))
+                return self.process_arg_spec(arg_spec)
+
+    def parameters(self, options, simulation_parameters):
+        program_specified_parameters = getattr(self.context,
+                self.parameters_name, {})
+        overriding_parameters = extract_subdictionary(
+                simulation_parameters, options)
+        program_specified_parameters.update(overriding_parameters)
+        return program_specified_parameters
+
+
+
+    def build(self, parameters, set_=False):
+        factory = self.factory()
+        options = self.options(factory)
+        parameters = self.parameters(options, parameters)
+        try:
+            instance = factory(**parameters)
+        except TypeError as e:
+            raise ComponentError(e)
+        if set_:
+            setattr(self.context, self.component_name, instance)
+        else:
+            return instance
+
+
+
---
+++ tests/test_component_creator.py
@@ -0,0 +1,149 @@
+import unittest
+from pynetsym.util.component_creator import ComponentCreator, ComponentError
+
+class AttrMock(object):
+    def __init__(self, **kwargs):
+        vars(self).update(**kwargs)
+
+
+class AbstractComponentCreatorT(object):
+    def setUp(self):
+        self.component_name = 'foo'
+        self.options = ['a', 'b']
+        self.values = [1, 2]
+
+
+    def override_options(self):
+        return dict(zip(self.options, self.values))
+
+    def test_component_creator_set(self):
+        self.component_creator.build(self.override_options(),
+                                     set_=True)
+        self.assertTrue(hasattr(self.context, self.component_name))
+        component = getattr(self.context, self.component_name)
+
+        for option, value in zip(self.options, self.values):
+            self.assertEqual(value, getattr(component, option))
+
+    def test_component_creator(self):
+        component = self.component_creator.build(self.override_options())
+        for option, value in zip(self.options, self.values):
+            self.assertEqual(value, getattr(component, option))
+
+
+class TestComponentCreatorOptionsInContext(AbstractComponentCreatorT, unittest.TestCase):
+    def setUp(self):
+        super(TestComponentCreatorOptionsInContext, self).setUp()
+        self.context = AttrMock(
+                foo_options=self.options[:],
+                foo_type=AttrMock)
+        self.component_creator = ComponentCreator(
+                self.context,
+                self.component_name)
+
+class TestComponentCreatorOptionsInObject(AbstractComponentCreatorT, unittest.TestCase):
+    def setUp(self):
+        super(TestComponentCreatorOptionsInObject, self).setUp()
+        class SomeClass(AttrMock):
+            options = self.options[:]
+
+        self.context = AttrMock(foo_type=SomeClass)
+        self.component_creator = ComponentCreator(
+                self.context,
+                self.component_name)
+
+class TestComponentCreatorOptionsSomeStatic(AbstractComponentCreatorT, unittest.TestCase):
+    def setUp(self):
+        super(TestComponentCreatorOptionsSomeStatic, self).setUp()
+
+        self.options.append('c')
+        class SomeClass(AttrMock):
+            options = self.options[:]
+
+        self.context = AttrMock(
+                foo_type=SomeClass,
+                foo_parameters=dict(zip(self.options[:-1], self.values)))
+        self.component_creator = ComponentCreator(
+                self.context,
+                self.component_name)
+        self.values[1:] = (3, 4)
+
+    def override_options(self):
+        return dict(zip(self.options[1:], self.values[1:]))
+
+class TestComponentCreatorIntrospection(AbstractComponentCreatorT, unittest.TestCase):
+    def setUp(self):
+        super(TestComponentCreatorIntrospection, self).setUp()
+
+        class SomeClass(AttrMock):
+            def __init__(self, a, b):
+                super(SomeClass, self).__init__(a=a, b=b)
+
+        self.context = AttrMock(foo_type=SomeClass)
+        self.component_creator = ComponentCreator(
+            self.context, self.component_name)
+
+
+
+class TestComponentCreatorNotEnoughArguments(unittest.TestCase):
+    def setUp(self):
+        self.component_name = 'foo'
+        self.options = ['a', 'b']
+        self.values = [1, 2]
+
+        class SomeClass(AttrMock):
+            def __init__(self, a, b, c):
+                super(SomeClass, self).__init__(a=a, b=b, c=c)
+
+        self.context = AttrMock(foo_type=SomeClass)
+        self.component_creator = ComponentCreator(
+            self.context, self.component_name)
+
+    def test_component_creator(self):
+        self.assertRaises(ComponentError, self.component_creator.build,
+                          self.override_options())
+
+    def override_options(self):
+        return dict(zip(self.options, self.values))
+
+class TestComponentCreatorAdditional(unittest.TestCase):
+    def setUp(self):
+        self.component_name = 'foo'
+        self.options = ['a', 'b']
+        self.values = [1, 2]
+
+        class SomeClass(AttrMock):
+            def __init__(self, a, *b):
+                super(SomeClass, self).__init__(a=a, b=b)
+
+        self.context = AttrMock(foo_type=SomeClass)
+        self.component_creator = ComponentCreator(
+            self.context, self.component_name)
+
+    def test_component_creator(self):
+        self.assertRaises(ComponentError, self.component_creator.build,
+                          self.override_options())
+
+    def override_options(self):
+        return dict(zip(self.options, self.values))
+
+class TestComponentCreatorKeyword(unittest.TestCase):
+    def setUp(self):
+        self.component_name = 'foo'
+        self.options = ['a', 'b']
+        self.values = [1, 2]
+
+        class SomeClass(AttrMock):
+            def __init__(self, a, **b):
+                super(SomeClass, self).__init__(a=a, b=b)
+
+        self.context = AttrMock(foo_type=SomeClass)
+        self.component_creator = ComponentCreator(
+            self.context, self.component_name)
+
+    def test_component_creator(self):
+        self.assertRaises(ComponentError, self.component_creator.build,
+                          self.override_options())
+
+    def override_options(self):
+        return dict(zip(self.options, self.values))
