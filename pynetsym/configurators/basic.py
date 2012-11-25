54311d14aa63c89a5fd339be0e1502a7c28f49ad
Enrico Franchi <efranchi@ce.unipr.it>
2012-11-25 16:15:28 +0100

    Minor cleanup.

--- pynetsym/configurators/basic.py
+++ pynetsym/configurators/basic.py
@@ -1,9 +1,8 @@
-import collections
 from traits.has_traits import implements
 from traits.trait_types import Dict, Str, Int, Any, Type, Set, false
 
 from .. import core
-from ..util import gather_from_ancestors, extract_subdictionary, SequenceAsyncResult
+from ..util import extract_subdictionary, SequenceAsyncResult
 from ..node_manager import NodeManager
 from .interface import IConfigurator
 
@@ -32,14 +31,6 @@ class AbstractConfigurator(core.Agent):
 
     node_identifiers = Any
     full_parameters = Dict(key_trait=Str)
-
-#    def __init__(self, **additional_arguments):
-#        full_options = gather_from_ancestors(
-#                self, 'configurator_options')
-#        configurator_arguments = extract_subdictionary(
-#                additional_arguments, full_options)
-#        self.set(**configurator_arguments)
-#        self.set(additional_arguments=additional_arguments)
 
     def _start(self):
         self.create_nodes()
